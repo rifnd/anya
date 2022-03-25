888b3f94f978ea37a9967ca99ff7fb5a8f3a7296
Nando <88189057+Nando35@users.noreply.github.com>
2022-03-25 10:38:08 +0700

    Update handhold.js

--- plugins/handhold.js
+++ plugins/handhold.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let res = await fetch(API('waifupics', '/sfw/handhold'))
   if (!res.ok) throw eror
   let json = await res.json()
-  await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), 'Nih kak', wm, 'Get Again', usedPrefix + command, m)
+  await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), 'Nih kak', watermark, 'Get Again', usedPrefix + command, m)
 }
 handler.help = ['handhold']
 handler.tags = ['anime']
