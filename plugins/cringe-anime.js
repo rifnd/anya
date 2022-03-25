042a8ea6e0cbb19e21086c4d6a74f0d041f397a6
Nando <88189057+Nando35@users.noreply.github.com>
2022-03-25 10:37:27 +0700

    Update cringe-anime.js

--- plugins/cringe-anime.js
+++ plugins/cringe-anime.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let res = await fetch(API('waifupics', '/sfw/cringe'))
   if (!res.ok) throw eror
   let json = await res.json()
-  await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), 'Nih kak', wm, 'Get Again', usedPrefix + command, m)
+  await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), 'Nih kak', watermark, 'Get Again', usedPrefix + command, m)
 }
 handler.help = ['cringe']
 handler.tags = ['anime']
