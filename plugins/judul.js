99cf4869da3953f2e36a8389734edee6a4baa11a
Nando <88189057+Nando35@users.noreply.github.com>
2022-03-27 07:12:38 +0700

    Update judul.js

--- plugins/judul.js
+++ plugins/judul.js
@@ -11,13 +11,13 @@ let handler = async (m) => {
   let json = await res.json()
   let caption = `
 Lagu Ditemukan!
+
 Judul: ${json.data.title}
 Artists: ${json.data.artist}
 Genre: ${json.data.genre}
 Album: ${json.data.album}
 Realese Date: ${json.data.relese_date}
-Note: Hasil dari api.zeks.xyz. Hasil bisa tidak akurat.
-©Haruno
+_Note: Hasil bisa tidak akurat._
 `.trim()
     conn.reply(m.chat, caption, m)
 }
