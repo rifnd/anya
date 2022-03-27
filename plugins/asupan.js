c77987684e38eb3c0f166a51e9a9669e06055f58
Nando <88189057+Nando35@users.noreply.github.com>
2022-03-27 19:01:43 +0700

    Update asupan.js

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,18 +1,9 @@
-let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-  conn.sendButtonVid(m.chat, API('amel', '/asupan', {}, 'apikey'), 'Tobat bang', watermark, 'Lagi', '#asupan', m)
+  conn.sendFile(m.chat, API('mel', '/asupan', {}, 'apikey'), 'asupan.mp4', '© melcanz x ariffb')
 }
 handler.help = ['asupan']
 handler.tags = ['fun']
-handler.command = /^(asupan)$/i
 handler.limit = 1
-
-module.exports = handler
-]
-  await conn.sendFile(m.chat, 'http://sansekai.my.id/ptl_repost/'+asupan, asupan, null, m)
-}
-handler.help = ['asupan']
-handler.tags = ['fun']
 handler.command = /^(asupan)$/i
 
 module.exports = handler
