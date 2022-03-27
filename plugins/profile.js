a9908356b4f304f76f65a0e8182b3a45f7907ad4
Nando <88189057+Nando35@users.noreply.github.com>
2022-03-27 07:17:19 +0700

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -23,8 +23,6 @@ let handler = async (m, { conn, usedPrefix }) => {
         banned: false,
         level: 0,
         call: 0,
-        dailyReward: 0,
-        joincount: 0,
         role: 'Hyaktaku V',
         autolevelup: false,
         pc: 0,
@@ -32,26 +30,27 @@ let handler = async (m, { conn, usedPrefix }) => {
         premiumTime: 0,
       }
     }
-    let { name, limit, exp, lastclaim, registered, regTime, age, level, role, banned, premium, premiumTime, joincount, dailyReward } = global.db.data.users[who]
+    let { name, limit, exp, lastclaim, registered, regTime, age, level, role, banned, premium, premiumTime } = global.db.data.users[who]
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let username = conn.getName(who)
     let math = max - xp
     let str = `
-Nama: ${username} ${registered ? '(' + name + ') ' : ''}(@${who.replace(/@.+/, '')})${about != 401 ? '\nInfo: ' + about : ''}
-Nomor: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-Link: https://wa.me/${who.split`@`[0]}
-XP: ${exp} 
-Level: ${level}
-Role: ${role}
-Limit: ${limit}
-Joincount: ${joincount}
-Daily Reward: ${dailyReward}
-Daftar: ${registered ? '✅' : '❌'}
-Premium: ${premium ? `✅\nPremium Expired: ${conn.msToDate(premiumTime - new Date() * 1)}` : '❌'}
-Banned: ${banned ? '✅' : '❌'}
+*Profile*
+
+- Nama: ${username} ${registered ? '(' + name + ') ' : ''}(@${who.replace(/@.+/, '')})${about != 401 ? '\nInfo: ' + about : ''}
+- Nomor: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+- Link: https://wa.me/${who.split`@`[0]}
+- XP: ${exp} 
+- Level: ${level}
+- Role: ${role}
+- Limit: ${limit}
+- Daftar: ${registered ? '✅' : '❌'}
+- Premium: ${premium ? `✅\nPremium Expired: ${conn.msToDate(premiumTime - new Date() * 1)}` : '❌'}
+- Banned: ${banned ? '✅' : '❌'}
+
 `.trim()
     let mentionedJid = [who]
-    conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid } })
+    conn.sendButtonLoc(m.chat, pp, str, m, watermark, 'Menu', '#menu', false, { contextInfo: { mentionedJid } })
   }
 }
 handler.help = ['profile [@62XXXX]']
